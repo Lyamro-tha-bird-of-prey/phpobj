fd23da6a2b70b3a970d9d1b24f66f04e83afc09f
WiRo96 <wilrob919@gmail.com>
2018-11-28 11:33:00 +0100

    modification dog et ajout extends

--- dog.php
+++ dog.php
@@ -1,6 +1,7 @@
 <?php
 class Dog {
     private $dateNaissance = "2000-01-01";
+    private $age;
     /**
      * @return string
      */
@@ -8,9 +9,17 @@ class Dog {
     {
         return $this->dateNaissance;
     }
+    //mes fonctions
+    public function setAge($dateNaissance) {
+       $this->age = date_diff($dateNaissance, date());
+    }
+    public function getAge() {
+      return date->format("Y");
+    }
 }
 $dog = new Dog();
 var_dump(strtotime($dog->getDateNaissance()));
 // Comparer la différence entre la date de naissance et la date actuel
 // Convertir en Nombre d'années
 // date()
+var_dump(strtotime($dog->getAge()));
---
+++ extends.php
@@ -0,0 +1,49 @@
+<?php
+class Animal {
+    private $year = 0;
+    private $sex = "NC";
+    public function getYear() {
+        return $this->year;
+    }
+    public function getSex() {
+        return $this->sex;
+    }
+    public function addYear() {
+        $this->year++;
+    }
+}
+class Mammal extends Animal {
+    private $color;
+    public function __construct($pColor)
+    {
+        $this->color = $pColor;
+    }
+}
+class Oviparous extends Animal {
+    private $color;
+    public function __construct($pColor)
+    {
+        $this->color = $pColor;
+    }
+}
+class Gecko extends Oviparous {
+    private $name;
+}
+class Person extends Mammal {
+    private $firstname;
+    private $lastname;
+    public function __construct($pFirstname, $pLastname, $pColor)
+    {
+        parent::__construct($pColor);
+        $this->firstname = $pFirstname;
+        $this->lastname = $pLastname;
+    }
+}
+class Dog extends Mammal {
+    private $name;
+}
+$person = new Person("john", "doe", "beige", 1, "male");
+$dog = new Dog("noir");
+$gecko = new Gecko("vert");
+var_dump($person);
+var_dump($gecko);
